e3bf7c8d7b8341be34f20193635c32b0e4aa4e8f
Mikey Ying <mikeyinguk@gmail.com>
2023-01-22 21:34:26 +0000

    created option to search for specific pokemon and a prompt to search by id or name wip

--- src/userPrompts.js
+++ src/userPrompts.js
@@ -9,13 +9,17 @@ const initialPrompt = async () => {
         message: 'What would you like to do?\n',
         choices: [
             'Get a random pokemon.',
+            'Get a specific pokemon.',
             'Quit',
         ]
     })
 
     switch (answers.initial_question) {
         case 'Get a random pokemon.':
-            whichRandomPokemon()
+            rangePrompt()
+            break
+        case 'Get a specific pokemon.':
+            searchMethodPrompt()
             break
         case 'Quit':
             process.exitCode = 0;
@@ -23,9 +27,9 @@ const initialPrompt = async () => {
     }
 }
 
-const whichRandomPokemon = async () => {
+const rangePrompt = async () => {
     const answers = await inquirer.prompt({
-        name: 'which_random_pokemon',
+        name: 'range_prompt',
         type: 'list',
         message: 'Which pokemon would you like to select from?\n',
         choices: [
@@ -37,7 +41,7 @@ const whichRandomPokemon = async () => {
 
     let fetchedPokemon
 
-    switch (answers.which_random_pokemon) {
+    switch (answers.range_prompt) {
 
         case 'All current pokemon.':
             fetchedPokemon = await getRandomPokemon(allCurrentPokemons)
@@ -53,4 +57,29 @@ const whichRandomPokemon = async () => {
     }
 }
 
-export { initialPrompt, whichRandomPokemon }
+const searchMethodPrompt = async () => {
+    const answers = await inquirer.prompt({
+        name: 'search_method_prompt',
+        type: 'list',
+        message: 'How would you like to search?\n',
+        choices: [
+            'Pokemon ID.',
+            'Pokemon name.',
+            'Quit',
+        ]
+    })
+
+    switch (answers.search_method_prompt) {
+        case 'Pokemon ID.':
+            console.log('search by id')
+            break
+        case 'Pokemon name.':
+            console.log('search by name')
+            break
+        case 'Quit':
+            process.exitCode = 0;
+            break
+    }
+}
+
+export { initialPrompt, rangePrompt }
