83d397c76a16ae8918906b1e6419266bed5ada3e
Mikey Ying <mikeyinguk@gmail.com>
2023-01-22 22:20:55 +0000

    created error message function

--- src/fetches/getPokemon.js
+++ src/fetches/getPokemon.js
@@ -1,8 +1,10 @@
+import errorMessage from "../utilities/errorMessage.js"
+
 const getPokemonById = async (id) => {
 
     const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
     if (data.status === 404) {
-        console.log('Pokemon does not exist - invalid ID')
+        errorMessage('Pokemon does not exist - invalid ID')
     } else {
         const pokemon = await data.json()
         return pokemon
---
+++ src/utilities/errorMessage.js
@@ -0,0 +1,7 @@
+import chalk from "chalk";
+
+const errorMessage = (error) => {
+    console.log(chalk.bgRed(error))
+}
+
+export default errorMessage
