e462a45c7bd4cc3daa3f845693d0fbf17a9e49d6
Mikey Ying <mikeyinguk@gmail.com>
2023-01-22 22:32:01 +0000

    added check for user input type. converts string to lowercase

--- src/fetches/getPokemon.js
+++ src/fetches/getPokemon.js
@@ -1,14 +1,19 @@
 import errorMessage from "../utilities/errorMessage.js"
 
-const getPokemonById = async (id) => {
+const getPokemonByIdOrName = async (userInput) => {
 
-    const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    if (typeof(userInput === 'string')) {
+        userInput = userInput.toLowerCase()
+    }
+
+    const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${userInput}`)
+    
     if (data.status === 404) {
-        errorMessage('Pokemon does not exist - invalid ID')
+        errorMessage('Pokemon does not exist - invalid ID or name')
     } else {
         const pokemon = await data.json()
         return pokemon
     }
 }
 
-export { getPokemonById }
+export { getPokemonByIdOrName }
