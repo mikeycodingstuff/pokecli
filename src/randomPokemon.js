454cc1c7f5871a2b28e8d18434ee8e59929b6e0e
Mikey Ying <mikeyinguk@gmail.com>
2023-01-22 19:45:31 +0000

    created function to get current number of total pokemon via api

--- src/randomPokemon.js
+++ src/randomPokemon.js
@@ -1,13 +1,26 @@
 import randomIntInclusive from "./utilities/randomIntInclusive.js"
 
-const originalPokemons = { min: 1, max: 151 }
+const getNumberOfTotalCurrentPokemon = async () => {
+    const data = await fetch(`https://pokeapi.co/api/v2/pokedex/1`)
+    const pokedexInfo = await data.json()
 
-const getRandomPokemon = async () => {
-    const id = randomIntInclusive(originalPokemons)
+    return pokedexInfo.pokemon_entries.length
+}
+
+const getRandomPokemon = async (pokemonRange) => {
+    const id = randomIntInclusive(pokemonRange)
     const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
     const pokemon = await data.json()
 
     return pokemon
 }
 
-export default getRandomPokemon
+const originalPokemons = { min: 1, max: 151 }
+const allCurrentPokemons = { min: 1, max: await getNumberOfTotalCurrentPokemon() }
+
+export {
+    getNumberOfTotalCurrentPokemon,
+    getRandomPokemon,
+    originalPokemons,
+    allCurrentPokemons,
+}
