1c3502e4edded71bd6c61d809eb64e36dd88ad91
Mikey Ying <mikeyinguk@gmail.com>
2023-08-13 15:37:08 +0100

    styling changes

--- src/welcome.js
+++ src/welcome.js
@@ -4,16 +4,12 @@ import sleep from './utilities/sleep.js'
 const mainColor = '#7b62ac'
 
 const welcome = async () => {
-    console.clear()
-
     console.log(`
-
 
         ${chalk.bgHex(mainColor)('POKEMON CLI')}
         by ${chalk.hex(mainColor)('Mikey')}
 
         A command line tool that shows pokemon info by consuming the pokemon API (found at https://pokeapi.co/)
-
 
     `)
 
