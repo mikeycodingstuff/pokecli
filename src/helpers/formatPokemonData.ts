3eda4cb2ac40b948dd68655b35deacd6e78ce824
Mikey Ying <mikeyinguk@gmail.com>
2023-08-14 23:21:47 +0100

    remove unused format

--- src/helpers/formatPokemonData.ts
+++ src/helpers/formatPokemonData.ts
@@ -23,20 +23,4 @@ const formatPokemonListData = async () => {
 	}
 };
 
-const formatRandomPokemonData = async () => {
-	try {
-		const pokemon = await api.getRandomPokemon();
-
-		return pokemon;
-	} catch (error) {
-		if (error instanceof ApiError) {
-			displayError(`Error fetching Pokemon: ${error.message}`);
-		} else {
-			displayError('Unknown error occurred while fetching Pokemon');
-		}
-
-		process.exit(1);
-	}
-};
-
-export { formatPokemonListData, formatRandomPokemonData };
+export { formatPokemonListData };
