c386223edcb70cfa17b7b20073cf1bf5cf6b1ffa
Mikey Ying <mikeyinguk@gmail.com>
2023-08-22 21:43:39 +0100

    remove comments and duplicate test

--- test/display/chalkErrorMessage.test.ts
+++ test/display/chalkErrorMessage.test.ts
@@ -1,26 +1,11 @@
-// test/display/chalkErrorMessage.test.ts
 import chalkErrorMessage from '../../src/helpers/display/chalkErrorMessage';
 
-// Mock chalk module
 jest.mock('chalk', () => ({
 	bgRed: (text: string): string => `background red: ${text}`,
 }));
 
 describe('chalkErrorMessage', () => {
-	it('should apply background red using chalk.bgRed', () => {
-		const message = 'Error message';
-		const result = chalkErrorMessage(message);
-		expect(result).toBe('background red: Error message');
-	});
-});
-
-// Mock chalk module
-jest.mock('chalk', () => ({
-	bgRed: (text: string): string => `background red: ${text}`,
-}));
-
-describe('chalkErrorMessage', () => {
-	it('should apply background red using chalk.bgRed', () => {
+	it('should return a message with background red styling', () => {
 		const message = 'Error message';
 		const result = chalkErrorMessage(message);
 		expect(result).toBe('background red: Error message');
