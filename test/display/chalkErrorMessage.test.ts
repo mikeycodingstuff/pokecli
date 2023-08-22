e58477e06ff64df7f1329d485a7d2650c66f0d4f
Mikey Ying <mikeyinguk@gmail.com>
2023-08-22 22:36:29 +0100

    add more chalk error message tests

--- test/display/chalkErrorMessage.test.ts
+++ test/display/chalkErrorMessage.test.ts
@@ -8,6 +8,34 @@ describe('chalkErrorMessage', () => {
 	it('should return a message with background red styling', () => {
 		const message = 'Error message';
 		const result = chalkErrorMessage(message);
-		expect(result).toBe('background red: Error message');
+
+		expect(result).toBe('background red: ' + message);
+	});
+
+	it('should return an empty string for an empty message', () => {
+		const result = chalkErrorMessage('');
+
+		expect(result).toBe('background red: ');
+	});
+
+	it('should properly style a message with special characters', () => {
+		const message = 'Error: [Critical]';
+		const result = chalkErrorMessage(message);
+
+		expect(result).toBe('background red: ' + message);
+	});
+
+	it('should style each line of a multi-line message', () => {
+		const message = 'Line 1\nLine 2\nLine 3';
+		const result = chalkErrorMessage(message);
+
+		expect(result).toBe('background red: ' + message);
+	});
+
+	it('should style a message with Unicode characters', () => {
+		const message = '🔴 Error 🔴';
+		const result = chalkErrorMessage(message);
+
+		expect(result).toBe('background red: ' + message);
 	});
 });
