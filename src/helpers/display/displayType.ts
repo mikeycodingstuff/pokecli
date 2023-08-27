400574dcc9662d62e8106d46566217037a3d73c3
Mikey Ying <mikeyinguk@gmail.com>
2023-08-27 22:42:40 +0100

    simplify code

--- src/helpers/display/displayType.ts
+++ src/helpers/display/displayType.ts
@@ -2,10 +2,8 @@ import { PokemonType } from '../../types.js';
 import { displayType } from '../display/displayPokemon.js';
 
 const displayTypeList = (formattedTypes: PokemonType[]): void => {
-	const typeNames = formattedTypes.map((type: PokemonType) => type.name);
-
-	for (const name of typeNames) {
-		console.log(displayType(name));
+	for (const type of formattedTypes) {
+		console.log(displayType(type.name));
 	}
 };
 
