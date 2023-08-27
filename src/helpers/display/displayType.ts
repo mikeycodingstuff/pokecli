6145f37cc3f10e84f5d04bca5edad5acf93226c5
Mikey Ying <mikeyinguk@gmail.com>
2023-08-27 22:41:31 +0100

    display types in column

--- src/helpers/display/displayType.ts
+++ src/helpers/display/displayType.ts
@@ -2,12 +2,11 @@ import { PokemonType } from '../../types.js';
 import { displayType } from '../display/displayPokemon.js';
 
 const displayTypeList = (formattedTypes: PokemonType[]): void => {
-	const typeNames = formattedTypes.map((type: PokemonType) =>
-		displayType(type.name),
-	);
+	const typeNames = formattedTypes.map((type: PokemonType) => type.name);
 
-	const joinedTypeNames = typeNames.join(', ');
-	console.log(` ${joinedTypeNames} `);
+	for (const name of typeNames) {
+		console.log(displayType(name));
+	}
 };
 
 export { displayTypeList };
