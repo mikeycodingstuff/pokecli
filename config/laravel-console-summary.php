014549fc3580594d75b86306a95e5ecc09bdcdf2
Mikey Ying <mikeyinguk@gmail.com>
2023-09-03 15:12:25 +0100

    publish summary, hide make and app commands

---
+++ config/laravel-console-summary.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Hide Commands
+    |--------------------------------------------------------------------------
+    |
+    | This option allows to hide certain commands from the summary output.
+    | They will still be available in your application. Wildcards are supported
+    |
+    | Examples: "make:*", "list"
+    |
+    */
+
+    'hide' => [
+        'make:*',
+        'app:*',
+        'list'
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Binary Name
+    |--------------------------------------------------------------------------
+    |
+    | This option allows to override the Artisan binary name that is used
+    | in the command usage output.
+    |
+    */
+
+    'binary' => null,
+
+];
