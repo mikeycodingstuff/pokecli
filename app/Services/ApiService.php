a094cceb9f2b6325a25569a47f6f64b7962992f9
Mikey Ying <mikeyinguk@gmail.com>
2023-09-03 21:34:26 +0100

    method to get highest pokemon id

--- app/Services/ApiService.php
+++ app/Services/ApiService.php
@@ -31,4 +31,22 @@ class ApiService
 
         return null;
     }
+
+    private static function getHighestPokemonId()
+    {
+        $base_url = config('api.urls.base_url');
+        $url = $base_url."pokedex/1";
+
+        $response = Http::get($url);
+
+        try {
+            $data = $response->json();
+
+            $lastEntry = $data['pokemon_entries'][count($data['pokemon_entries']) - 1];
+            
+            return $lastEntry['entry_number'];
+        } catch (Exception $e) {
+
+        }
+    }
 }
