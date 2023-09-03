0250da14cd152794aad775a874f26566cf728298
Mikey Ying <mikeyinguk@gmail.com>
2023-09-03 15:44:00 +0100

    clean up command

--- app/Commands/Search.php
+++ app/Commands/Search.php
@@ -4,33 +4,15 @@ namespace App\Commands;
 
 use App\Controllers\PokemonController;
 use App\Helpers\ViewHelper;
-use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Support\Facades\Validator;
 use LaravelZero\Framework\Commands\Command;
 
-use function Termwind\{render};
-
 class Search extends Command
 {
-    /**
-     * The signature of the command.
-     *
-     * @var string
-     */
     protected $signature = 'search {query : The Pokémon name or ID}';
 
-    /**
-     * The description of the command.
-     *
-     * @var string
-     */
     protected $description = 'Search for info on a Pokémon';
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
     public function handle(PokemonController $pokemonController)
     {
         $query = $this->argument('query');
@@ -58,13 +40,5 @@ class Search extends Command
 
             return 1;
         }
-    }
-
-    /**
-     * Define the command's schedule.
-     */
-    public function schedule(Schedule $schedule): void
-    {
-        // $schedule->command(static::class)->everyMinute();
     }
 }
