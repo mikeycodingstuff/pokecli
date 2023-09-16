c36c651b90f4b5cd89fb5cee2de5cf3786e90256
Mikey Ying <mikeyinguk@gmail.com>
2023-09-16 14:31:51 +0100

    clean up welcome

--- app/Commands/Welcome.php
+++ app/Commands/Welcome.php
@@ -6,31 +6,15 @@ namespace App\Commands;
 
 use App\Helpers\ViewHelper;
 use App\Services\StyleManager;
-use Illuminate\Console\Scheduling\Schedule;
 use LaravelZero\Framework\Commands\Command;
 
 class Welcome extends Command
 {
-    /**
-     * The signature of the command.
-     *
-     * @var string
-     */
-    protected $signature = 'welcome';
+    protected string $signature = 'welcome';
 
-    /**
-     * The description of the command.
-     *
-     * @var string
-     */
-    protected $description = 'Print welcome';
+    protected string $description = 'Print welcome';
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
-    public function handle()
+    public function handle(): int
     {
         $mainColor = config('colors.app.mainColor.hex');
         $replaceColorClass = config('colors.app.mainColor.replace');
@@ -40,13 +24,5 @@ class Welcome extends Command
         ViewHelper::renderView('welcome', ['bgColor' => $bgColor]);
 
         return self::SUCCESS;
-    }
-
-    /**
-     * Define the command's schedule.
-     */
-    public function schedule(Schedule $schedule): void
-    {
-        // $schedule->command(static::class)->everyMinute();
     }
 }
