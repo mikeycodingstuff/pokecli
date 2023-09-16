c6b4358dbf1051e747adb42d661d8234460c739b
Mikey Ying <mikeyinguk@gmail.com>
2023-09-17 00:28:17 +0100

    fix test - need to change this to mock api request

--- tests/Feature/InfoCommandTest.php
+++ tests/Feature/InfoCommandTest.php
@@ -3,5 +3,5 @@
 declare(strict_types=1);
 
 it('displays pokemon info', function () {
-    $this->artisan('info')->assertExitCode(0);
+    $this->artisan('info', ['query' => 'metagross'])->assertExitCode(0);
 });
