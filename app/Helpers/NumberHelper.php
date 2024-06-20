b14716f65cd9c13b8188a281a91512d6630f8714
Mikey Ying <mikeyinguk@gmail.com>
2024-06-20 22:28:10 +0100

    move methods to separate helper file

---
+++ app/Helpers/NumberHelper.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Helpers;
+
+class NumberHelper
+{
+    public static function hectogramToKilogram(float $HgWeight): float
+    {
+        return round($HgWeight * 0.1, 2);
+    }
+
+    public static function decimetreToMetre(float $DmHeight): float
+    {
+        return round($DmHeight * 0.1, 2);
+    }
+}
