82dc494acb4f90f1808f9002f44ebd117b6f0528
Mikey Ying <mikeyinguk@gmail.com>
2024-06-20 23:28:18 +0100

    add national dex url to config

--- config/api.php
+++ config/api.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 return [
     'urls' => [
         'base_url' => 'https://pokeapi.co/api/v2/',
+        'national_dex' => 'https://pokeapi.co/api/v2/pokedex/1',
         'gen_list' => 'https://pokeapi.co/api/v2/generation?limit=100000&offset=0',
         'pokemon_list' => 'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0',
         'type_list' => 'https://pokeapi.co/api/v2/type?limit=100000&offset=0',
