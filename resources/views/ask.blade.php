908b60dc57e1fb27cbdf4fd3223a319f8ca8bdd9
Mikey Ying <mikeyinguk@gmail.com>
2024-06-21 09:15:08 +0100

    padding and margins

--- resources/views/ask.blade.php
+++ resources/views/ask.blade.php
@@ -5,11 +5,11 @@
 @endphp
 
 <div>
-    <span class="mt-1 ml-2 mr-1 bg-{{ $mainColor }}">
+    <span class="mt-1 ml-2 mr-1 bg-{{ $mainColor }} px-1">
         {{ $question }}
     </span>
 
-    <span class="mt-1 ml-2 mr-1">
+    <span class="mt-2 ml-2 mr-1">
         >
     </span>
 </div>
