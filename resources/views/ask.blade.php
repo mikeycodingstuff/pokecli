2b3dad09fb9a8db35328bd5266403fc53cf106bf
Mikey Ying <mikeyinguk@gmail.com>
2024-06-21 21:40:28 +0100

    refactor/styling

--- resources/views/ask.blade.php
+++ resources/views/ask.blade.php
@@ -1,15 +1,11 @@
 @props(['question', 'styles'])
 
-@php
-    $mainColor = $styles['mainColor'];
-@endphp
-
-<div>
-    <span class="mt-1 ml-2 mr-1 bg-{{ $mainColor }} px-1">
+<div class="mt-1 ml-1 mr-1">
+    <span class="bg-{{ $styles['primaryColor'] }} px-1">
         {{ $question }}
     </span>
 
-    <span class="mt-2 ml-2 mr-1">
+    <span class="mt-2">
         >
     </span>
 </div>
