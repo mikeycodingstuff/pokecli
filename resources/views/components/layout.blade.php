01bb9593ec4e7b6b2b18bf592e7f6d6f9b4edbdf
Mikey Ying <mikeyinguk@gmail.com>
2024-06-23 11:20:13 +0100

    add bottom margin

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,7 +6,7 @@
         <title></title>
     </head>
 
-    <div class="text-{{$textColor}} bg-{{$bgColor}}">
+    <div class="text-{{$textColor}} bg-{{$bgColor}} mb-1">
 	    {{ $slot }}
     </div>
 </html>
